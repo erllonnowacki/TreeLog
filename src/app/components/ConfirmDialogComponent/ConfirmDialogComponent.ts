3a4414a2803d663078276f2fba83b066edf6ee9e
Erllon <erllonemanuel11@gmail.com>
2023-03-05 21:29:59 -0300

    Alteração no layout

--- src/app/components/ConfirmDialogComponent/ConfirmDialogComponent.ts
+++ src/app/components/ConfirmDialogComponent/ConfirmDialogComponent.ts
@@ -6,7 +6,10 @@ import { MatDialogRef } from '@angular/material/dialog'
     template: `
         <h1 mat-dialog-title>Confirmação</h1>
         <div mat-dialog-content>Tem certeza que deseja excluir este item?</div>
-        <div mat-dialog-actions>
+        <div
+            mat-dialog-actions
+            style="display: flex; justify-content: space-between;"
+        >
             <button mat-button [mat-dialog-close]="false">Cancelar</button>
             <button mat-button [mat-dialog-close]="true">Excluir</button>
         </div>
